bc1aaa737a5807873adacb927565abecd17be6c0
haleybarlar <hbarlar1@gmail.com>
2018-12-16 11:52:00 -0500

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require('express')
 let bodyParser = require('body-parser')
+let {ObjectID} = require('mongodb')
 
 let {mongoose} = require('./db/mongoose')
 let {Todo} = require('./models/todo')
@@ -26,6 +27,34 @@ app.get('/todos', (req, res) => {
     res.send({todos: todos})
   }, (err) => {
     res.status(400).send(e)
+  })
+})
+
+// GET /todos/:id
+// validate id using .isValid
+// if not valid, stop execution and respond w 404 and send back empty value
+
+// findById
+  // success
+    // if todo - send back
+    // if no todo -  send back 404 w empty body
+  // error
+    // 404 - and send empty body back
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+
+    res.send({todo})
+  }).catch((e) => {
+    res.status(400).send()
   })
 })
 
